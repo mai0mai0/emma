f0fcbda5f9a63bc6427e43a7718252b4fe03c9a9
mai0mai0 <114875937+mai0mai0@users.noreply.github.com>
2023-09-13 23:34:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+//set date to countdown to
+var countDownDate  = new Date("Oct 6, 2023 17:40").getTime();
+
+var x = setInterval(function(){
+    //get current date
+    var now = new Date().getTime(); 
+
+    //difference between countDownDate and current date
+    var c = countDownDate - now; 
+
+    //calculations for days, hrs, mins, seconds
+    var days = Math.floor(c / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((c % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((c % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((c % (1000 * 60)) / 1000);
+
+
+    //update html p timer with info
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+
+    document.getElementById("cat").src = "./gifs/" + days + ".gif";
+
+    //display message on countdown finish
+    if (distance < 0){
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "I can finally see you again!!! I missed you so much <3"
+        document.getElementById("cat").src = "./gifs/finalday.gif";
+    }
+
+}, 1000);
+//update every second
